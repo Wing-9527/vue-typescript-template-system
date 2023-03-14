8c5fa7135d49066459db255c464f35ac39169430
haoyang.ding <2698300641@qq.com>
2023-03-14 12:47:24 +0800

    feat: save main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,36 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
+
+import TDesign from 'tdesign-vue-next'
+
 import '@/styles/global.scss'
 
+import router from './router'
+
+// import AxiosPlugin from './http'
+
+import { createPinia } from 'pinia'
+
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+// mockjs
+import './mock'
+
 const app = createApp(App)
+
+const pinia = createPinia()
+
+app.use(router)
+
+app.use(TDesign)
+
+// app.use(AxiosPlugin)
+
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+
+import './router/permission'
+
 app.mount('#app')
