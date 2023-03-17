b559725b6853fe54ce0fd127b0fce5edfe3060e1
haoyang.ding <2698300641@qq.com>
2023-03-17 21:31:42 +0800

    refactor: rewite load env method

--- config/loadEnv.js
+++ config/loadEnv.js
@@ -1,8 +1,15 @@
 /** @func 加载环境 */
+// function loadEnv(info) {
+//   let directive = info['npm_lifecycle_event']
+//   if (directive.includes(':')) directive = directive.replace(':', '_')
+//   let matchResult =
+//     info[`npm_package_scripts_${directive}`].match(/mode=(?<env>\w+)/)
+//   return matchResult ? matchResult.groups.env : ''
+// }
+
 function loadEnv(info) {
-  let directive = info['npm_lifecycle_event']
-  if (directive.includes(':')) directive = directive.replace(':', '_')
-  let matchResult = info[`npm_package_scripts_${directive}`].match(/mode=(?<env>\w+)/)
+  let directive = info['npm_lifecycle_script']
+  let matchResult = directive.match(/mode=(?<env>\w+)/)
   return matchResult ? matchResult.groups.env : ''
 }
 
