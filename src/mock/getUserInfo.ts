6cceb72b0ccad7b0d25322c0e7cf87fe39182e36
haoyang.ding <2698300641@qq.com>
2023-03-22 01:21:50 +0800

    feat: 修改mock数据

--- src/mock/getUserInfo.ts
+++ src/mock/getUserInfo.ts
@@ -3,15 +3,34 @@ import Mock from 'mockjs'
 Mock.mock('/mock/getUserInfo', 'post', () => {
   return {
     code: 200,
-    data: [
-      {
-        path: '/index',
-        name: 'Index',
-      },
-      {
-        path: '/home',
-        name: 'Home',
-      },
-    ],
+    data: {
+      name: 'admin',
+      role: 'admin',
+      description: '拥有所有权限',
+      permissions: [
+        {
+          name: 'Home',
+          path: '/home',
+          permission: 'Home',
+        },
+        {
+          name: 'SystemManage',
+          path: '/systemManege',
+          permission: 'SystemManage',
+          children: [
+            {
+              name: 'AccountManage',
+              path: '/systemManege/accountManage',
+              permission: 'AccountManage',
+            },
+            // {
+            //   name: 'RoleManage',
+            //   path: '/systemManege/roleManage',
+            //   permission: 'RoleManage',
+            // },
+          ],
+        },
+      ],
+    },
   }
 })
