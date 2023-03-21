cfc7d29b5849ab012fc01466fa7737aff517fe3d
haoyang.ding <2698300641@qq.com>
2023-03-22 01:23:39 +0800

    feat: 添加页面布局文件

---
+++ src/layout/BasicLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <t-layout>
+    <t-aside>
+      <t-menu theme="light" default-value="2-1" height="550px">
+        <template #logo>
+          <t-image
+            fit="cover"
+            :src="Logo"
+            :style="{ width: '232px', height: '56px' }"
+          />
+        </template>
+        <t-menu-item value="item1">
+          <template #icon>
+            <t-icon name="dashboard" />
+          </template>
+          仪表盘1
+        </t-menu-item>
+      </t-menu>
+    </t-aside>
+    <t-layout>
+      <t-header>Header</t-header>
+      <t-content>
+        <RouterView />
+      </t-content>
+      <t-footer>Footer</t-footer>
+    </t-layout>
+  </t-layout>
+</template>
+
+<script setup lang="ts">
+const Logo = require('@/assets/logo/logo.png')
+</script>
+
+<style lang="scss" scoped></style>
