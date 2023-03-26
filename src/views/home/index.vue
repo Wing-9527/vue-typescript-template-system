5e773578faef979ba807e9218161032f46b4295c
haoyang.ding <2698300641@qq.com>
2023-03-27 00:41:21 +0800

    feat: home页数据展示

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,367 @@
 <template>
-  <div>首页</div>
+  <div class="charts">
+    <div class="chart charts--first"></div>
+    <div class="chart charts--second"></div>
+    <div class="chart charts--third"></div>
+    <div class="chart charts--fourth"></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import * as echarts from 'echarts'
 
-<style lang="scss" scoped></style>
+let firstChartOption = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'line',
+    },
+  ],
+}
+
+let secontChartOption = {
+  title: {
+    text: 'Stacked',
+  },
+  tooltip: {
+    trigger: 'axis',
+  },
+  legend: {
+    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine'],
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+    },
+  },
+  xAxis: {
+    type: 'category',
+    boundaryGap: false,
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  },
+  yAxis: {
+    type: 'value',
+  },
+  series: [
+    {
+      name: 'Email',
+      type: 'line',
+      stack: 'Total',
+      data: [120, 132, 101, 134, 90, 230, 210],
+    },
+    {
+      name: 'Union Ads',
+      type: 'line',
+      stack: 'Total',
+      data: [220, 182, 191, 234, 290, 330, 310],
+    },
+    {
+      name: 'Video Ads',
+      type: 'line',
+      stack: 'Total',
+      data: [150, 232, 201, 154, 190, 330, 410],
+    },
+    {
+      name: 'Direct',
+      type: 'line',
+      stack: 'Total',
+      data: [320, 332, 301, 334, 390, 330, 320],
+    },
+    {
+      name: 'Search Engine',
+      type: 'line',
+      stack: 'Total',
+      data: [820, 932, 901, 934, 1290, 1330, 1320],
+    },
+  ],
+}
+
+let thirdChartOption = {
+  legend: {},
+  tooltip: {
+    trigger: 'axis',
+    showContent: false,
+  },
+  dataset: {
+    source: [
+      ['product', '2012', '2013', '2014', '2015', '2016', '2017'],
+      ['Milk Tea', 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+      ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+      ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+      ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1],
+    ],
+  },
+  xAxis: { type: 'category' },
+  yAxis: { gridIndex: 0 },
+  grid: { top: '55%' },
+  series: [
+    {
+      type: 'line',
+      smooth: true,
+      seriesLayoutBy: 'row',
+      emphasis: { focus: 'series' },
+    },
+    {
+      type: 'line',
+      smooth: true,
+      seriesLayoutBy: 'row',
+      emphasis: { focus: 'series' },
+    },
+    {
+      type: 'line',
+      smooth: true,
+      seriesLayoutBy: 'row',
+      emphasis: { focus: 'series' },
+    },
+    {
+      type: 'line',
+      smooth: true,
+      seriesLayoutBy: 'row',
+      emphasis: { focus: 'series' },
+    },
+    {
+      type: 'pie',
+      id: 'pie',
+      radius: '30%',
+      center: ['50%', '25%'],
+      emphasis: {
+        focus: 'self',
+      },
+      label: {
+        formatter: '{b}: {@2012} ({d}%)',
+      },
+      encode: {
+        itemName: 'product',
+        value: '2012',
+        tooltip: '2012',
+      },
+    },
+  ],
+}
+
+let fourthChartOption = {
+  color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
+  // title: {
+  //   text: 'Gradient Stacked Area Chart',
+  // },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross',
+      label: {
+        backgroundColor: '#6a7985',
+      },
+    },
+  },
+  legend: {
+    data: ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5'],
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {},
+    },
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true,
+  },
+  xAxis: [
+    {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+  ],
+  yAxis: [
+    {
+      type: 'value',
+    },
+  ],
+  series: [
+    {
+      name: 'Line 1',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0,
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(128, 255, 165)',
+          },
+          {
+            offset: 1,
+            color: 'rgb(1, 191, 236)',
+          },
+        ]),
+      },
+      emphasis: {
+        focus: 'series',
+      },
+      data: [140, 232, 101, 264, 90, 340, 250],
+    },
+    {
+      name: 'Line 2',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0,
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(0, 221, 255)',
+          },
+          {
+            offset: 1,
+            color: 'rgb(77, 119, 255)',
+          },
+        ]),
+      },
+      emphasis: {
+        focus: 'series',
+      },
+      data: [120, 282, 111, 234, 220, 340, 310],
+    },
+    {
+      name: 'Line 3',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0,
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(55, 162, 255)',
+          },
+          {
+            offset: 1,
+            color: 'rgb(116, 21, 219)',
+          },
+        ]),
+      },
+      emphasis: {
+        focus: 'series',
+      },
+      data: [320, 132, 201, 334, 190, 130, 220],
+    },
+    {
+      name: 'Line 4',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0,
+      },
+      showSymbol: false,
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(255, 0, 135)',
+          },
+          {
+            offset: 1,
+            color: 'rgb(135, 0, 157)',
+          },
+        ]),
+      },
+      emphasis: {
+        focus: 'series',
+      },
+      data: [220, 402, 231, 134, 190, 230, 120],
+    },
+    {
+      name: 'Line 5',
+      type: 'line',
+      stack: 'Total',
+      smooth: true,
+      lineStyle: {
+        width: 0,
+      },
+      showSymbol: false,
+      label: {
+        show: true,
+        position: 'top',
+      },
+      areaStyle: {
+        opacity: 0.8,
+        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+          {
+            offset: 0,
+            color: 'rgb(255, 191, 0)',
+          },
+          {
+            offset: 1,
+            color: 'rgb(224, 62, 76)',
+          },
+        ]),
+      },
+      emphasis: {
+        focus: 'series',
+      },
+      data: [220, 302, 181, 234, 210, 290, 150],
+    },
+  ],
+}
+
+onMounted(() => {
+  let firstChart = echarts.init(document.querySelector('.charts--first')!)
+  firstChart.setOption(firstChartOption)
+
+  let secondChart = echarts.init(document.querySelector('.charts--second')!)
+  secondChart.setOption(secontChartOption)
+
+  let thirdChart = echarts.init(document.querySelector('.charts--third')!)
+  thirdChart.setOption(thirdChartOption)
+
+  let fourthChart = echarts.init(document.querySelector('.charts--fourth')!)
+  fourthChart.setOption(fourthChartOption)
+  ;[firstChart, secondChart, thirdChart, fourthChart].forEach((chart) => {
+    chart.resize({ height: 300 })
+  })
+})
+</script>
+
+<style lang="scss" scoped>
+.charts {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  .chart {
+    width: 650px;
+    height: 50%;
+  }
+}
+</style>
