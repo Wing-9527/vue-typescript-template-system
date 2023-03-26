7f1273a3141eb6350f73c3d61a52de94883cd621
haoyang.ding <2698300641@qq.com>
2023-03-27 00:40:20 +0800

    feat: 头部用户下拉操作栏组件

---
+++ src/layout/GlobalHeader/components/AvatarDropdown.vue
@@ -0,0 +1,45 @@
+<template>
+  <t-dropdown
+    :options="[{ content: '退出登录', value: 'logout' }]"
+    @click="handleDropdownClick"
+  >
+    <div class="avatar-dropdown">
+      <t-avatar shape="round" size="large">
+        <template #icon>
+          <t-image
+            fit="cover"
+            :src="userInfoStore.avatar"
+            :style="{ width: '40px', height: '40px' }"
+          />
+        </template>
+      </t-avatar>
+      <span class="avatar-dropdown__username">{{ userInfoStore.name }}</span>
+    </div>
+  </t-dropdown>
+</template>
+
+<script setup lang="ts">
+import { useUserInfoStore } from '@/store/userInfo'
+import { useLoginStore } from '@/store/login'
+
+const userInfoStore = useUserInfoStore()
+const loginStore = useLoginStore()
+
+function handleDropdownClick(dropdownItem) {
+  if (dropdownItem.value === 'logout') {
+    loginStore.reqLogout()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.avatar-dropdown {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  &__username {
+    margin-left: 10px;
+  }
+}
+</style>
