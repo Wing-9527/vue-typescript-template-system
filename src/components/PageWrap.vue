0e00318c1ef9a12415db078b77d89ca068f3f799
haoyang.ding <2698300641@qq.com>
2023-03-27 00:39:12 +0800

    feat: 页面容器组件

---
+++ src/components/PageWrap.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.page-wrap {
+  padding: 15px;
+}
+</style>
