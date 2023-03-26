1ca85e3f2582754f22f2c3d11b785879cc92ed4b
haoyang.ding <2698300641@qq.com>
2023-03-27 00:15:48 +0800

    feat: 添加登出功能

--- src/store/login.ts
+++ src/store/login.ts
@@ -1,5 +1,7 @@
 import { defineStore } from 'pinia'
 
+import router from '@/router'
+
 import { reqLoginApi } from '@/api/login/login.api'
 
 export const useLoginStore = defineStore('login', {
@@ -7,6 +9,10 @@ export const useLoginStore = defineStore('login', {
     async reqLogin(data: Record<string, string>) {
       let res = await reqLoginApi(data)
       sessionStorage.setItem('token', res.data)
+    },
+    reqLogout() {
+      sessionStorage.clear()
+      router.replace('/login')
     },
   },
 })
