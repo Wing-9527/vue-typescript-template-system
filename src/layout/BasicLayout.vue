80093b5a219e3fd64ce3756e400735a50fa1fa02
haoyang.ding <2698300641@qq.com>
2023-03-27 00:38:42 +0800

    feat: 布局组件修改

--- src/layout/BasicLayout.vue
+++ src/layout/BasicLayout.vue
@@ -1,13 +1,13 @@
 <template>
   <t-layout>
-    <t-aside>
-      <t-menu theme="light" default-value="2-1" height="550px">
+    <t-aside :width="collapsed ? '80px' : '200px'">
+      <t-menu :collapsed="collapsed" :width="['200px', '80px']">
         <template #logo>
-          <t-image
-            fit="cover"
+          <img
             :src="Logo"
-            :style="{ width: '232px', height: '56px' }"
+            :style="{ width: '32px', height: '32px', marginLeft: '24px' }"
           />
+          <span v-show="!collapsed" class="system-name">Vue admin</span>
         </template>
         <div v-for="item of renderMenu" :key="item.path" :value="item.path">
           <t-submenu
@@ -34,21 +34,32 @@
             {{ item.meta?.title }}
           </t-menu-item>
         </div>
+        <template #operations>
+          <t-button
+            class="t-demo-collapse-btn"
+            variant="text"
+            shape="square"
+            @click="handleCollapsedChange"
+          >
+            <template #icon><t-icon :name="iconName" /></template>
+          </t-button>
+        </template>
       </t-menu>
     </t-aside>
     <t-layout>
-      <t-header>Header</t-header>
+      <GlobalHeader />
       <t-content>
         <RouterView />
       </t-content>
-      <!-- <t-footer>Footer</t-footer> -->
     </t-layout>
   </t-layout>
 </template>
 
 <script setup lang="ts">
-import { useUserInfoStore } from '@/store/userInfo'
+import { ref, computed } from 'vue'
 import router from '@/router'
+import { useUserInfoStore } from '@/store/userInfo'
+import GlobalHeader from './GlobalHeader/index.vue'
 
 const Logo = require('@/assets/logo/logo.png')
 
@@ -56,10 +67,29 @@ const userInfoStore = useUserInfoStore()
 const renderMenu = userInfoStore.menu[0].children // 渲染的菜单
 
 const pathPrefix = router.options.history.base ? '/#' : '' // 判断是hash路由还是history路由，给路径加前缀
+
+// 菜单展开收起
+const collapsed = ref(false)
+
+const iconName = computed(() => (collapsed.value ? 'menu-fold' : 'menu-unfold'))
+
+function handleCollapsedChange() {
+  collapsed.value = !collapsed.value
+}
 </script>
 
 <style lang="scss" scoped>
 .t-layout {
   height: 100%;
+}
+:deep() .t-menu__logo {
+  background-color: #304156;
+}
+.system-name {
+  margin-left: 10px;
+  font-family: 'INKFREE';
+  font-size: large;
+  font-weight: bold;
+  color: #ffffff;
 }
 </style>
