2e23e4391e847753642273f0f00b19781b616468
haoyang.ding <2698300641@qq.com>
2023-03-27 00:40:43 +0800

    feat: 表单容器组件

---
+++ src/components/FormContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="form-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.form-container {
+  margin-bottom: 20px;
+}
+</style>
