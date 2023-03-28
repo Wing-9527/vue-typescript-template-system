5d7941b003ad5e4aa7e59cc245e7ec372dc7c394
haoyang.ding <2698300641@qq.com>
2023-03-28 23:44:20 +0800

    feat: 更新mock数据

--- src/mock/getUserInfo.ts
+++ src/mock/getUserInfo.ts
@@ -21,14 +21,14 @@ let adminPermission = {
           path: '/systemManege/roleManage',
           permission: 'RoleManage',
           actionEntitySet: [
-            // {
-            //   action: 'edit',
-            //   describe: '编辑',
-            // },
-            // {
-            //   action: 'permission',
-            //   describe: '权限',
-            // },
+            {
+              action: 'edit',
+              describe: '编辑',
+            },
+            {
+              action: 'permission',
+              describe: '权限',
+            },
             {
               action: 'delete',
               describe: '删除',
@@ -55,36 +55,36 @@ let adminPermission = {
   ],
 }
 
-// let guestPermission = {
-//   name: 'admin',
-//   role: 'admin',
-//   avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-//   description: '拥有所有权限',
-//   permissions: [
-//     {
-//       name: 'Home',
-//       path: '/home',
-//       permission: 'Home',
-//     },
-//     {
-//       name: 'SystemManage',
-//       path: '/systemManege',
-//       permission: 'SystemManage',
-//       children: [
-//         {
-//           name: 'AccountManage',
-//           path: '/systemManege/accountManage',
-//           permission: 'AccountManage',
-//         },
-//         {
-//           name: 'RoleManage',
-//           path: '/systemManege/roleManage',
-//           permission: 'RoleManage',
-//         },
-//       ],
-//     },
-//   ],
-// }
+let guestPermission = {
+  name: 'guest',
+  role: 'guest',
+  avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+  description: '拥有所有权限',
+  permissions: [
+    {
+      name: 'Home',
+      path: '/home',
+      permission: 'Home',
+    },
+    {
+      name: 'SystemManage',
+      path: '/systemManege',
+      permission: 'SystemManage',
+      children: [
+        {
+          name: 'AccountManage',
+          path: '/systemManege/accountManage',
+          permission: 'AccountManage',
+        },
+        {
+          name: 'RoleManage',
+          path: '/systemManege/roleManage',
+          permission: 'RoleManage',
+        },
+      ],
+    },
+  ],
+}
 
 Mock.mock('/mock/getUserInfo', 'post', () => {
   return {
